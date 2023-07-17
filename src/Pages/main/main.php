d327a48827feca354a084eeb8d29907d8a0e0361
jose <josenoboa1992@gmail.com>
2023-07-17 17:10:44 -0400

    actualizando-agregar-fichero-company-3

---
+++ src/Pages/main/main.php
@@ -0,0 +1,71 @@
+
+<section class="section m-3 p-3">
+<div class="row">
+    <div class="col-lg-3 mb-3">
+
+        <div class="card text-gray">
+            <div class="card-body">
+                <i class="fas fa-tag"></i>
+                <div class="saving">
+                    <p >Ventas</p>
+                    <h5 class=""> RD$ 455,377.62</h5>
+                    <p><span> Balance disponible</span></p>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="col-lg-3 mb-3">
+
+        <div class="card text-gray">
+            <div class="card-body">
+                <i class="bi bi -piggy-bank"></i>
+                <div class="saving">
+                    <p>Cuentas de ahorros</p>
+                    <h1 class="private"> RD$ 455,377.62</h1>
+                    <p><span> Balance disponible</span></p>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="col-lg-3 mb-3">
+
+        <div class="card text-gray">
+            <div class="card-body">
+                <i class="bi bi-piggy-bank"></i>
+                <div class="saving">
+                    <p>Cuentas de ahorros</p>
+                    <h1 class="private"> RD$ 455,377.62</h1>
+                    <p><span> Balance disponible</span></p>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="col-lg-3 mb-3">
+
+        <div class="card text-gray">
+            <div class="card-body">
+                <i class="bi bi-piggy-bank"></i>
+                <div class="saving">
+                    <p>Cuentas de ahorros</p>
+                    <h1 class="private"> RD$ 455,377.62</h1>
+                    <p><span> Balance disponible</span></p>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+</section>
